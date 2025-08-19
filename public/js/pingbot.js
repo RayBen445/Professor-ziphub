1e3bc37d59b6949bda55bd4c32762b71fc37d243
jamesdevke <eaglegnickme@gmail.com>
2025-08-19 16:19:01 +0300

    Add files via upload

--- public/js/pingbot.js
+++ public/js/pingbot.js
@@ -1,8 +1,12 @@
 // pings server to keep app alive forever (client-side)
 (function keepAlive(){
   async function ping(){
-    try { await fetch('/ping'); } catch(e){}
-    setTimeout(ping, 5 * 60 * 1000); // every 5 minutes
+    try { 
+      await fetch('/ping'); 
+    } catch(e) {
+      console.error("Ping failed:", e);
+    }
+    setTimeout(ping, 5000); // every 5 seconds
   }
   ping();
 })();
